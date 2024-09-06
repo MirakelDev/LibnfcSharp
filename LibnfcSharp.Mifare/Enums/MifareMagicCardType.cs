86a41a959ddc2582aa5ce3cae62170032bf01231
MirakelDev <118356358+MirakelDev@users.noreply.github.com>
2024-09-06 12:46:04 +0200

    - add descriptions to enum MifareMagicCardType

--- LibnfcSharp.Mifare/Enums/MifareMagicCardType.cs
+++ LibnfcSharp.Mifare/Enums/MifareMagicCardType.cs
@@ -1,10 +1,19 @@
-﻿namespace LibnfcSharp.Mifare.Enums
+﻿using System.ComponentModel;
+
+namespace LibnfcSharp.Mifare.Enums
 {
     public enum MifareMagicCardType
     {
+        [Description("Gen1 UID LOCKED")]
         NONE,
+
+        [Description("Gen1A UID Changeable (Backdoor)")]
         GEN_1A,
+
+        [Description("Gen1B UID Changeable (Backdoor)")]
         GEN_1B,
+
+        [Description("Gen2 CUID")]
         GEN_2
     }
 }
